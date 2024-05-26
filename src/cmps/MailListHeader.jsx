ac2d4a807d96a9d5c1a45f652bef89d5451d52e3
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-05-26 13:36:14 +0300

    Add actions to header

--- src/cmps/MailListHeader.jsx
+++ src/cmps/MailListHeader.jsx
@@ -1,20 +1,28 @@
 import { IoRefreshOutline } from "react-icons/io5";
 import { IoMdMore } from "react-icons/io";
+import { MailActions } from "./MailActions.jsx";
 
+export function MailListHeader({ reloadMails, handleSelectAll, checkIds, handleUnread, handleDelete }) {
+    const goBack = () => {
+        history.back()
+    }
 
-export function MailListHeader() {
     return (
         <div className="mail-list-header">
-
             <div className=".mail-details-btn-no-hover">
-                <input type='checkbox' />
+                <input type='checkbox' onChange={(e) => handleSelectAll(e)} />
             </div>
-            <div className="mail-details-btn">
-                <IoRefreshOutline />
-            </div>
-            <div className="mail-details-btn">
-                <IoMdMore />
-            </div>
+            {
+                checkIds.length > 0 ? <MailActions goBack={goBack} handleDelete={handleDelete} handleUnread={handleUnread} /> : <>
+                    <div className="mail-details-btn" onClick={reloadMails}>
+                        <IoRefreshOutline />
+                    </div>
+                    <div className="mail-details-btn">
+                        <IoMdMore />
+                    </div>
+                </>
+            }
+
         </div>
     )
 }
