11a90f2e9efabc2a74baf0d1430b5e3a6cd08bd1
Nachliel <nachpok@gmail.com>
2024-05-29 02:45:41 +0300

    Update MailIndex.jsx

--- src/cmps/MailIndex.jsx
+++ src/cmps/MailIndex.jsx
@@ -6,10 +6,11 @@ import { AppHeader } from "./AppHeader.jsx";
 import { SideBar } from "./SideBar.jsx";
 import { ComposeMailModal } from "./ComposeMailModal.jsx";
 import { Loader } from "./Loader.jsx";
+import { useNavigate } from "react-router-dom";
+import { utilService } from "../services/util.service.js";
 
 export function MailIndex() {
-
-  //TODO null optional
+  const navigate = useNavigate();
   const [mails, setMails] = useState([]);
   const [unreadCounters, setUnreadCounters] = useState({
     inbox: 0,
@@ -42,12 +43,60 @@ export function MailIndex() {
     fetchMails();
   }, [location]);
 
-  const onSearchChange = (e) => {
+  function onSearchChange(e) {
     setSearchValue(e.target.value);
   };
 
+  async function onUpdateSelectedMails(action, ids) {
+    let selectedMails = mails.filter((mail) => ids.includes(mail.id));
+
+    switch (action) {
+      case 'delete':
+        selectedMails = selectedMails.filter((mail) => mail.removedAt === null);
+        for (let mail of selectedMails) {
+          mail.removedAt = new Date();
+          await mailService.updateMail(mail);
+        }
+        break;
+
+      case 'unread':
+        selectedMails = selectedMails.filter((mail) => mail.isRead === true);
+        for (let mail of selectedMails) {
+          mail.isRead = false;
+          await mailService.updateMail(mail);
+        }
+        break;
+
+      case 'archive':
+        selectedMails = selectedMails.filter((mail) => mail.isArchived === false);
+        for (let mail of selectedMails) {
+          mail.isArchived = true;
+          await mailService.updateMail(mail);
+        }
+        break;
+
+      case 'star':
+        selectedMails = selectedMails.filter((mail) => mail.isStarred === false);
+        for (let mail of selectedMails) {
+          mail.isStarred = true;
+          await mailService.updateMail(mail);
+        }
+        break;
+
+      default:
+        throw new Error('Invalid action type');
+    }
+
+    if (isMailDetailsRoute()) {
+      setLoading(true);
+      const folder = location.pathname.split("/")[1];
+      navigate('/' + folder);
+    } else {
+      await fetchMails();
+    }
+  }
   //Close compose mail modal and reload mails when sent is in view
-  const onComposeMailModal = async (isOpen) => {
+  async function onComposeMailModal(isOpen) {
     if (isOpen === false && currentUrl.includes("/sent")) {
       await fetchMails();
     }
@@ -74,11 +123,12 @@ export function MailIndex() {
             <Loader />
           ) : (
             isMailDetailsRoute() ? (
-              <Outlet context={{ reloadMails: fetchMails }} />
+              <Outlet context={{ onUpdateSelectedMails }} />
             ) : (
               <MailList
                 mails={mails}
                 reloadMails={fetchMails}
+                onUpdateSelectedMails={onUpdateSelectedMails}
               />
             )
           )}
