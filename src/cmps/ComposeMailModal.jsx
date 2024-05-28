a0c8e3167019ac5ac05a4050c7c617ead9b4050c
Nachliel <nachpok@gmail.com>
2024-05-28 23:57:54 +0300

    compose mail semantic refactoring

--- src/cmps/ComposeMailModal.jsx
+++ src/cmps/ComposeMailModal.jsx
@@ -27,32 +27,31 @@ export function ComposeMailModal({ closeComposeMailModal }) {
             const answer = confirm("Send this message without a subject or text in the body?");
             if (!answer) return;
         }
-        await mailService.createMail({ to: toRef.current.value, subject: subjectRef.current.value, body: messageRef.current.value })
-        closeComposeMailModal(false)
+        console.log("messageRef.current.value: \n", messageRef.current.value)
+        // await mailService.createMail({ to: toRef.current.value, subject: subjectRef.current.value, body: messageRef.current.value })
+        // closeComposeMailModal(false)
     };
 
     return (
         <article className='compose-mail-modal'>
-            <header className='compose-mail-modal-header'>
-                <h1 className='compose-mail-modal-title'>New Message</h1>
-                <button className='compose-mail-modal-close-btn' onClick={() => closeComposeMailModal(false)}>X</button>
+            <header className='header'>
+                <h1 className='title'>New Message</h1>
+                <button className='close-btn' onClick={() => closeComposeMailModal(false)}>X</button>
             </header>
-            <main className='compose-mail-modal-body'>
-                <form className='compose-mail-modal-form'>
-                    <input type="email" placeholder='To' className={`compose-mail-modal-form-item compose-mail-modal-form-input`} ref={toRef} />
-                    <input type="text" placeholder='Subject' className={`compose-mail-modal-form-item compose-mail-modal-form-input`} ref={subjectRef} />
-                    <textarea placeholder='Message' className={`compose-mail-modal-form-item compose-mail-modal-form-textarea`} ref={messageRef}></textarea>
-                </form>
-            </main>
-            <footer className='compose-mail-modal-footer'>
-                <button className='compose-mail-modal-send-btn' onClick={onSendMail}>Send</button>
+            <form className='form'>
+                <input type="email" placeholder='To' className={`form-item form-input`} ref={toRef} />
+                <input type="text" placeholder='Subject' className={`form-item form-input`} ref={subjectRef} />
+                <textarea placeholder='Message' className={`form-item form-textarea`} ref={messageRef}></textarea>
+            </form>
+            <footer className='footer'>
+                <button className='send-btn' onClick={onSendMail}>Send</button>
             </footer>
             {errorModal &&
-                <article className="compose-mail-modal-error-modal">
-                    <main className="compose-mail-modal-error-modal-content">
+                <article className="compose-mail-modal error-modal">
+                    <main className="error-modal-content">
                         <h1>Error</h1>
                         <p>Please enter a valid email address.</p>
-                        <button onClick={() => setErrorModal(false)} className="compose-mail-modal-error-modal-close-btn">Close</button>
+                        <button onClick={() => setErrorModal(false)} className="modal-close-btn">Close</button>
                     </main>
                 </article>
             }
