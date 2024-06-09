d29b7d3824adc7468e3c275908a4dab0161afdb8
Nachliel <nachpok@gmail.com>
2024-06-09 18:40:07 +0300

    cleanup app header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,11 @@
 import { HomeButton } from "./HomeButton";
 import { SearchDropdown } from "./SearchDropdown";
+
 export function AppHeader({ fetchMailsByText, fetchMailsByAdvancedSearch }) {
-
-
   return (
     <header className="app-header">
       <HomeButton />
       <SearchDropdown fetchMailsByText={fetchMailsByText} fetchMailsByAdvancedSearch={fetchMailsByAdvancedSearch} />
-
     </header>
   );
 }
