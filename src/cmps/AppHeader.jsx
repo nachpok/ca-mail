1b10c1581f4e2b8d7bcad19122251179f148a3b7
Nachliel <nachpok@gmail.com>
2024-06-17 18:53:41 +0300

    add link btn

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,12 +2,15 @@ import { HomeButton } from "./HomeButton";
 import { SearchDropdown } from "./SearchDropdown";
 import { IoMenu } from "react-icons/io5";
 import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { IoMdHelpCircleOutline } from "react-icons/io";
 
 export function AppHeader({ fetchMailsByText, fetchMailsByAdvancedSearch, onSideBarToggle }) {
   const [isMenuBtnFocus, setIsMenuBtnFocus] = useState(false);
   const [isMouseOverMenu, setIsMouseOverMenu] = useState(false);
-
+  const { folder } = useParams();
   const isMenuBtnActive = isMenuBtnFocus || isMouseOverMenu;
+
   return (
     <header className="app-header">
       <button
@@ -22,6 +25,9 @@ export function AppHeader({ fetchMailsByText, fetchMailsByAdvancedSearch, onSide
       </button>
       <HomeButton />
       <SearchDropdown fetchMailsByText={fetchMailsByText} fetchMailsByAdvancedSearch={fetchMailsByAdvancedSearch} onSideBarToggle={onSideBarToggle} />
+      <Link to={`/${folder}?compose=new&to=help@gmail.com&subject=Help`} className="help-btn">
+        <IoMdHelpCircleOutline size={24} />
+      </Link>
     </header>
   );
 }
